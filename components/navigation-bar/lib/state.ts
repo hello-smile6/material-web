fb5ec2ea7ca27e3044a16c4912ab060095822ef9
Esteban Gonzalez <esgonzalez@google.com>
2022-03-17 15:16:48 -0700

    refactor(navbar): Remove foundation/adapter logic.
    PiperOrigin-RevId: 435461808

--- components/navigation-bar/lib/state.ts
+++ components/navigation-bar/lib/state.ts
@@ -1,15 +1,15 @@
 /**
  * @license
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {MDCNavigationTabState} from 'google3/third_party/javascript/material_web_components/m3/navigation_tab/lib/state';
+import {NavigationTabState} from 'google3/third_party/javascript/material_web_components/m3/navigation_tab/lib/state';
 
 /**
  * The state of a navigation Bar.
  */
-export interface MDCNavigationBarState {
+export interface NavigationBarState {
   /**
    * Index of the active navigation tab.
    */
@@ -21,31 +21,5 @@ export interface MDCNavigationBarState {
   /**
    * An array of the navigation tab states.
    */
-  tabs: MDCNavigationTabState[];
-}
-
-/**
- * Defines the shape of the adapter expected by the foundation.
- *
- * This adapter is used to delegate state-only updates from the foundation
- * to the component. It does not delegate DOM or rendering logic, such as adding
- * or removing classes.
- */
-export interface MDCNavigationBarAdapter {
-  /**
-   * The state of the component.
-   */
-  state: MDCNavigationBarState;
-  /**
-   * Focuses the navigation tab at the given index.
-   */
-  focusTab(index: number): void;
-  /**
-   * Returns the index of the focused navigation tab.
-   */
-  getFocusedTabIndex(): number;
-  /**
-   * Returns true if the text direction is right-to-left.
-   */
-  isRTL(): boolean;
+  tabs: NavigationTabState[];
 }
